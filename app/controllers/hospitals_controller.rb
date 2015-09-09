91d027c5da3523715859a8c3418936fd8ad50270
JM <limjungmok@gmail.com>
2015-09-09 16:39:40 +0900

    json parsing complete

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,5 +1,12 @@
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+require 'net/http'
+require 'httparty'
+
 class HospitalsController < ApplicationController
-	
+
+
 	def new
 	end
 
@@ -7,6 +14,19 @@ class HospitalsController < ApplicationController
 	end
 
 	def show
+		type="/json/"
+		search_location = "gangnam"
+		service_key = "70554f5a78636e643739784d584f62"
+		location_hospital = "Gn"
+
+		base_url = "http://openapi."+search_location+
+					".go.kr:8088/"+service_key+type+location_hospital+
+					"AnimalHospital/1/1000"
+		#http://openapi.gangnam.go.kr:8088/70554f5a78636e643739784d584f62/json/GnAnimalHospital/1/1000
+	    
+	    @response = HTTParty.get(base_url)
+	    @http_party_json = JSON.parse(@response.body)["GnAnimalHospital"]["row"]
+	    @h_name = @http_party_json.length
 	end
 
 	def index
@@ -20,4 +40,19 @@ class HospitalsController < ApplicationController
 
 	def destroy
 	end
+
+	def get_json
+	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -2,4 +2,8 @@ class Hospital < ActiveRecord::Base
 	has_many :albums
 	has_many :reservations
 	has_many :talks
+
+
+
+	
 end
