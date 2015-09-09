75d3ddcf14abd4781325f1f4d4e356ec4a0e8b32
JM <limjungmok@gmail.com>
2015-09-10 00:32:25 +0900

    jm

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -14,19 +14,8 @@ class HospitalsController < ApplicationController
 	end
 
 	def show
-		type="/json/"
-		search_location = "gangnam"
-		service_key = "70554f5a78636e643739784d584f62"
-		location_hospital = "Gn"
-
-		base_url = "http://openapi."+search_location+
-					".go.kr:8088/"+service_key+type+location_hospital+
-					"AnimalHospital/1/1000"
-		#http://openapi.gangnam.go.kr:8088/70554f5a78636e643739784d584f62/json/GnAnimalHospital/1/1000
-	    
-	    @response = HTTParty.get(base_url)
-	    @http_party_json = JSON.parse(@response.body)["GnAnimalHospital"]["row"]
-	    @h_name = @http_party_json.length
+		service_key_gangnam = "70554f5a78636e643739784d584f62"
+		get_json(service_key_gangnam)
 	end
 
 	def index
@@ -41,7 +30,20 @@ class HospitalsController < ApplicationController
 	def destroy
 	end
 
-	def get_json
+	def get_json(get_service_key)
+		type="/json/"
+		search_location = "gangnam"
+		service_key = get_service_key
+		location_hospital = "Gn"
+
+		base_url = "http://openapi."+search_location+
+					".go.kr:8088/"+service_key+type+location_hospital+
+					"AnimalHospital/1/1000"
+		#http://openapi.gangnam.go.kr:8088/70554f5a78636e643739784d584f62/json/GnAnimalHospital/1/1000
+	    
+	    @response = HTTParty.get(base_url)
+	    @http_party_json = JSON.parse(@response.body)["GnAnimalHospital"]["row"]
+	    @h_name = @http_party_json.length		
 	end
 end
 
