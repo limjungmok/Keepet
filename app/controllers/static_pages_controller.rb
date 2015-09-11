82c29e73814da1a58d2f0c82a4fa6260c2873f31
JM <limjungmok@gmail.com>
2015-09-11 14:33:48 +0900

    jm_x

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,16 @@
 class StaticPagesController < ApplicationController
   
   def home
-  end
-
-  def map
   	service_key_gangnam = "70554f5a78636e643739784d584f62"
 		
 		#동물병원 API
 		get_json(service_key_gangnam)
 		#동물병원 주소 -> x,y API
 		#get_json_location(nhn_service_key)
+  end
+
+  def map
+  	
 		
   end
   	
