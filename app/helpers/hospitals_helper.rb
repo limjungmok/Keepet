88126cd6b5f6c0a4773058578bb9ef8a4ce64578
JM <limjungmok@gmail.com>
2015-09-17 20:56:46 +0900

    jm

--- app/helpers/hospitals_helper.rb
+++ app/helpers/hospitals_helper.rb
@@ -8,5 +8,11 @@ module HospitalsHelper
 			@test.save
 		end
 	end
+  def meetings_ajax_previous_link
+    ->(param, date_range) { link_to raw("<<"), {param => date_range.first - 1.day}, remote: :true}
+  end
 
+  def meetings_ajax_next_link
+    ->(param, date_range) { link_to raw(">>"), {param => date_range.last + 1.day}, remote: :true}
+  end
 end
