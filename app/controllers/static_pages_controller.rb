5e0b1475038b6fe4aafa77ec89237d19232691b8
coco <ysj08240@gmail.com>
2015-10-09 14:43:43 +0900

    20151009

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
-class StaticPagesController < ApplicationController
-	
+class StaticPagesController < ApplicationController	
 	def home
-	
+		@hospital_1 = Hospital.find(1)
+		@hospital_2 = Hospital.find(3)
+		@hospital_3 = Hospital.find(4)
+		@hospital_4 = Hospital.find(5)
+		@hospital_5 = Hospital.find(6)
+		@hospital_6 = Hospital.find(7)
+		@hospital_7 = Hospital.find(8)
+		@hospital_8 = Hospital.find(9)
 	end
-	
 end
